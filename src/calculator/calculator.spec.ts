4fb3a03ad79df7119e141d79686e330f5e688ee5
Diego Consen <diego.consen@navara.nl>
2022-07-14 13:02:24 +0200

    Calculator Add method return sum when two numbers

--- src/calculator/calculator.spec.ts
+++ src/calculator/calculator.spec.ts
@@ -3,9 +3,13 @@ import {Add} from "./calculator";
 describe('Calculator', () => {
     it('should return 0 on empty string', function () {
         expect(Add('')).toBe(0);
-    })
+    });
 
     it('should return self when only number', function () {
-        expect(Add('3')).toBe(3)
+        expect(Add('3')).toBe(3);
+    });
+
+    it('should return sum when two numbers seperated by comma', function () {
+        expect(Add('3,4')).toBe(7);
     });
 })
--- src/calculator/calculator.ts
+++ src/calculator/calculator.ts
@@ -1,3 +1,6 @@
 export function Add(numbers: string): number {
-    return !!numbers ? parseInt(numbers) : 0;
+    if (numbers === '') return 0;
+
+    const parsedNumbers = numbers.split(',').map(Number);
+    return parsedNumbers.reduce((prev, curr) => prev + curr, 0);
 }
